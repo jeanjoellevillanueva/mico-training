fdc16fc53d1d6724e0cdaff2789b90d811793594
Mico <mico.agapito28@gmail.com>
2026-05-14 01:56:10 +0800

    Added the about/ url

--- rango/urls.py
+++ rango/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from rango import views
 
 app_name = 'rango'
-
+name = 'about'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
