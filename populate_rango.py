8ded684617176f2b8d32ccef678ab96cce951579
Mico <mico.agapito28@gmail.com>
2026-05-22 01:53:01 +0800

    Chapter 6 exercise

--- populate_rango.py
+++ populate_rango.py
@@ -14,25 +14,33 @@ def populate():
 
     python_pages = [
         {'title': 'Official Python Tutorial',
-        'url':'http://docs.python.org/3/tutorial/'},
+        'url':'http://docs.python.org/3/tutorial/',
+        'views': 100},
         {'title':'How to Think like a Computer Scientist',
-        'url':'http://www.greenteapress.com/thinkpython/'},
+        'url':'http://www.greenteapress.com/thinkpython/',
+        'views': 50},
         {'title':'Learn Python in 10 Minutes',
-        'url':'http://www.korokithakis.net/tutorials/python/'} ]
+        'url':'http://www.korokithakis.net/tutorials/python/',
+        'views': 25} ]
     
     django_pages = [
         {'title':'Official Django Tutorial',
-        'url':'https://docs.djangoproject.com/en/4.0/intro/tutorial01/'},
+        'url':'https://docs.djangoproject.com/en/4.0/intro/tutorial01/',
+        'views': 100},
         {'title':'Django Rocks',
-        'url':'http://www.djangorocks.com/'},
+        'url':'http://www.djangorocks.com/',
+        'views': 50},
         {'title':'How to Tango with Django',
-        'url':'http://www.tangowithdjango.com/'} ]
+        'url':'http://www.tangowithdjango.com/',
+        'views': 25} ]
     
     other_pages = [
         {'title':'Bottle',
-        'url':'http://bottlepy.org/docs/dev/'},
+        'url':'http://bottlepy.org/docs/dev/',
+        'views': 100},
         {'title':'Flask',
-        'url':'http://flask.pocoo.org'} ]
+        'url':'http://flask.pocoo.org',
+        'views': 50} ]
     
     cats = {'Python': {'pages': python_pages},
             'Django': {'pages': django_pages},
@@ -58,8 +66,6 @@ def add_page(cat, title, url, views=0):
     # save = "write it into the database"
     # return = "give me the result back"
 
-# This code create page properly with the url and views fields,
-#  and it also uses the get_or_create method to avoid creating duplicate pages.
     p, created = Page.objects.get_or_create(
         category=cat,
         title=title,
--- rango/views.py
+++ rango/views.py
@@ -37,16 +37,19 @@ def show_category(request, category_name_slug):
 
 # Remember that the index() function is responsible for the main page view.
 def index(request):
-    # set up the context dictionary with our bold message.
-    context_dict = {'aboldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
 
     # Query the database for a list of ALL categories currently stored.
     # Order the categories by the number of likes in descending order.
     # Retrieve the top 5 only -- or all if less than 5. 
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
     # Place the list in our context_dict dictionary (with our boldmessage!)
     # that will be passed to the template engine.
-    context_dict['categories'] = category_list
+    context_dict = {
+        'categories': category_list,
+        'pages': page_list
+    }
+    
     # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
 
