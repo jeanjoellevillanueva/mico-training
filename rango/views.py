1701759702fea161b8f183a68d18d6975b35edf5
Mico <mico.agapito28@gmail.com>
2026-05-31 02:28:51 +0800

    Chapter 10: Exercise

--- rango/views.py
+++ rango/views.py
@@ -54,7 +54,9 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
+
+    # IMPORTANT: use session not cookies 
+    context_dict['visits'] = int(request.session.get('visits', '1'))
 
     # Obtain our Response object early so we can add cookie information.
     response = render(request, 'rango/index.html', context=context_dict)
@@ -75,7 +77,10 @@ def about(request):
         print("TEST COOKIE WORKED!")
         request.session.delete_test_cookie()
 
-    return render(request, 'rango/about.html', {})
+    # return render(request, 'rango/about.html', {})
+    visits = request.session.get('visits', 1)
+    return render(request, 'rango/about.html', {'visits': visits})
+
 
 
 # Creating an Add Category View
@@ -260,19 +265,24 @@ def visitor_cookies_handler(request, response):
         # We use the COOKIES.get() function to obtain the visits cookie.
         # If the cookie exists, the value returned is casted to an integer.
         # If the cookie doesn't exist, then the default value of 1 is used.
-        visits = int(request.COOKIES.get('visits', '1'))
+        visits = request.session.get('visits', '1')
 
-        last_visit_cookies = request.COOKIES.get('last_visit', str(datetime.now()))
-        last_visit_time = datetime.strptime(last_visit_cookies[:-7], '%Y-%m-%d %H:%M:%S')
+        last_visit = request.session.get('last_visit')
 
-        # If it's been more than a day since the last visit...
-        if (datetime.now() - last_visit_time).days > 0:
-            visits = visits + 1
-            # Update the last visit cookie now that we have updated the count
-            response.set_cookie('last_visit', str(datetime.now()))
+        current_time = datetime.now()
+
+        if last_visit:
+            last_visit_time = datetime.strptime(last_visit, '%Y-%m-%d %H:%M:%S')
+
+            # If more than a day passed
+            if (current_time - last_visit_time).days > 0:
+                visits += 1
+            
         else:
-            # Set the last visit cookie
-            response.set_cookie('last_visit', last_visit_cookies)   
+            visits = 1
+
         # Update/set the visits cookie
         request.session['visits'] = visits
+        request.session['last_visit'] = str(current_time)
+
         return response
